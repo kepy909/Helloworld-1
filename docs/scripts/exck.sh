df11f36a3f1adcbda811b7e550793740f90591e7
Super Manito <68613938+SuperManito@users.noreply.github.com>
2022-04-02 15:27:51 +0800

    Update

--- docs/scripts/exck.sh
+++ docs/scripts/exck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\nJD_COOKIE=\"${JD_COOKIE}\"\n"
+echo -e "\nexport JD_COOKIE=\"${JD_COOKIE}\"\n"
 echo "${JD_COOKIE}" | perl -pe '{s|&|\n|g}' | perl -pe '{s|^|\"|g, s|;$|;\",|g}'
 echo -e ''
